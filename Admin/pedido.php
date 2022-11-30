681f667fc8ef39478140f2a620f9892c360befb4
Johan Rojas <johanandresrojasnaranjo@gmail.com>
2022-11-30 07:27:57 -0500

    modeal de pedido megorado

--- Admin/pedido.php
+++ Admin/pedido.php
@@ -178,7 +178,7 @@
             <!-- Modal1 -->
             <div class="modal fade" id="crear" tabindex="-1" aria-labelledby="crear" aria-hidden="true">
                 <div class="modal-dialog">
-                    <div class="modal-content">
+                    <div class="modal-content wh">
                         <div class="modal-header">
                             <h1 class="titulo-Modal ">Crear</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -215,26 +215,26 @@
                                         <input type="search" name="nombre" placeholder="Buscar">
                                     </form>
                                     <ul>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"><br></li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"> <br></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
                                     </ul>
                                 </div>
 
                                 <div class="editados">
-                                    <ul>
-                                        <li class="edi2">Mango $2.000
-                                            <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#Insumo">
+                                <ul>
+                                        <li class="edi2">Mango $2.000 <input type="number" name="" id="" class="cantidad">
+                                            <button type="button" class="buttonn" data-bs-toggle="modal" data-bs-target="#Insumo">
                                                 <i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                                     </svg></i>
                                             </button>
                                         </li>
-                                        <li class="edi2">Mango $2.000
-                                            <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#Insumo">
+                                        <li class="edi2">Mango $2.000 <input type="number" name="" id="" class="cantidad">
+                                            <button type="button" class="buttonn" data-bs-toggle="modal" data-bs-target="#Insumo">
                                                 <i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
@@ -524,7 +524,7 @@
             <!-- Editar -->
             <div class="modal fade" id="editar" tabindex="-1" aria-labelledby="editar" aria-hidden="true">
                 <div class="modal-dialog">
-                    <div class="modal-content">
+                    <div class="modal-content   wh">
                         <div class="modal-header">
                             <h1 class="titulo-Modal ">Editar</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -562,26 +562,26 @@
                                         <input type="search" name="nombre" placeholder="Buscar">
                                     </form>
                                     <ul>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"><br></li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
-                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> </li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"> <br></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
+                                        <li class="edi">Mango $2.000<input type="checkbox" class="seletor1" id="cbox1" value="first_checkbox"> <input type="number" name="" id="" class="cantidad"></li>
                                     </ul>
                                 </div>
 
                                 <div class="editados">
                                     <ul>
-                                        <li class="edi2">Mango $2.000
-                                            <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#Insumo">
+                                        <li class="edi2">Mango $2.000 <input type="number" name="" id="" class="cantidad">
+                                            <button type="button" class="buttonn" data-bs-toggle="modal" data-bs-target="#Insumo">
                                                 <i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
                                                     </svg></i>
                                             </button>
                                         </li>
-                                        <li class="edi2">Mango $2.000
-                                            <button type="button" class="button" data-bs-toggle="modal" data-bs-target="#Insumo">
+                                        <li class="edi2">Mango $2.000 <input type="number" name="" id="" class="cantidad">
+                                            <button type="button" class="buttonn" data-bs-toggle="modal" data-bs-target="#Insumo">
                                                 <i class="bi bi-trash"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
                                                         <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z" />
