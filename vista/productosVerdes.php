22db685d38157646fc8755fc05b3712402a55aea
Diego <diegopulgonilza28@gmail.com>
2022-12-01 21:14:15 -0500

    Mejora del modal

--- vista/productosVerdes.php
+++ vista/productosVerdes.php
@@ -24,6 +24,132 @@
     require_once 'campo/Nav.php';
     ?>
 
+    <!-- Inicio de modales -->
+
+    <!-- Inicio de modal 1-->
+    <div class="modal fade" id="myModal">
+        <div class="modal-dialog modal-sm modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- titulo del modal -->
+                <div class="modal-header">
+                    <h2 class="modal-tittle md-3">
+                        Jugo
+                    </h2>
+                    <button class="btn btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <h2 class="fs-3 ">Licuado verde</h2>
+                    <!-- <a href="carrito.php"> -->
+                    <img class="rounded float-start" src="../IMG/licuadoVerde.jpg" alt="Una imagen de fresa">
+                    <!-- </a> -->
+                    <h1 class="fs-4 text-md-center">Descripción</h1>
+                    <p class="fs-5 text-md-center">
+                        Jugo muy bueno
+                    </p>
+                    <h5 class="fs-5 text-md-center">Precio: $40.00 - $80.00</h5>
+                </div>
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                            </svg>
+                        </a>
+                    </button>
+                    <button class="btn btn-danger" data-bs-dismiss="modal">
+                        Cerrar
+                    </button>
+                </div>
+                <!-- Fin del footer -->
+            </div>
+        </div>
+    </div>
+    <!-- Fin del modal 1-->
+
+    <!-- Inicio de modal 2-->
+    <div class="modal fade" id="myModal2">
+        <div class="modal-dialog modal-sm modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- titulo del modal -->
+                <div class="modal-header">
+                    <h2 class="modal-tittle ">
+                        Jugo
+                    </h2>
+                    <button class="btn btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <h2 class="fs-3 row">Jugo de manzana</h2>
+                    <img class="rounded md-center col-9" src="../IMG/jugoManzana.jpg" alt="Una imagen de fresa">
+                    <h1 class="fs-4 text-md-center">Descripción</h1>
+                    <p class="fs-5 text-md-center">
+                        Jugo muy bueno
+                    </p>
+                    <h5 class="fs-5 text-md-center"><span class="text-muted text-decoration-line-through">$20.00</span>
+                        $18.00</h5>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                            </svg>
+                        </a>
+                    </button>
+                    <button class="btn btn-danger" data-bs-dismiss="modal">
+                        Cerrar
+                    </button>
+                </div>
+                <!-- Fin de footer modal -->
+            </div>
+        </div>
+    </div>
+    <!-- Fin del modal 2-->
+
+    <!-- Inicio de modal 3 -->
+    <div class="modal fade" id="myModal3">
+        <div class="modal-dialog modal-sm modal-dialog-centered">
+            <div class="modal-content">
+
+                <!-- titulo del modal -->
+                <div class="modal-header">
+                    <h2 class="modal-tittle text-center">
+                        Jugo
+                    </h2>
+                    <button class="btn btn-close" data-bs-dismiss="modal"></button>
+                </div>
+                <div class="modal-body">
+                    <h2 class="fs-3 text-md-center">Jugo combinado</h2>
+                    <!-- <a href="carrito.php"> -->
+                    <img class="rounded float-start img-fluid" src="../IMG/jugoConvinado.jpg" alt="Una imagen de fresa">
+                    <!-- </a> -->
+                    <h1 class="fs-4 text-md-center">Descripción</h1>
+                    <p class="f-3 text-md-center">
+                        Jugo muy bueno
+                    </p>
+                    <h5 class="fs-5">$40.00</h5>
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                                <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
+                            </svg>
+                        </a>
+                    </button>
+                    <button class="btn btn-danger" data-bs-dismiss="modal">
+                        Cerrar
+                    </button>
+                </div>
+                <!-- fin del fooder -->
+            </div>
+        </div>
+    </div>
+    <!-- Fin del modal -->
+    <!-- fin de modales -->
     <!-- Section-->
     <section>
         <div>
@@ -31,8 +157,8 @@
         </div>
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                <!-- Primer  div -->
 
+                <!-- Primer  div -->
                 <div class="col mb-5">
                     <div class="card h-100">
                         <!-- Product image-->
@@ -51,51 +177,6 @@
                             <button class="btnn btnn-two ">
                                 <p class="text-boton">Ver más</p>
                             </button>
-                            <!-- Inicio de modal -->
-                            <div class="modal fade" id="myModal">
-                                <div class="modal-dialog modal-xl modal-dialog-centered">
-                                    <div class="modal-content">
-
-                                        <!-- titulo del modal -->
-                                        <div class="modal-header">
-                                            <h2 class="modal-tittle md-3">
-                                                Jugo
-                                            </h2>
-                                            <button class="btn btn-close" data-bs-dismiss="modal"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <h2 class="fs-3 col-md-3">Licuado verde</h2>
-                                            <!-- <a href="carrito.php"> -->
-                                            <img class="rounded float-start" src="../IMG/licuadoVerde.jpg" alt="Una imagen de fresa">
-                                            <!-- </a> -->
-                                            <h1 class="fs-4">Descripción</h1>
-                                            <p class="f-3">
-                                                Jugo muy bueno
-                                            </p>
-                                            <!-- Selector, no puedo moverlo al centro  -->
-                                            <!-- <select class="position-relative form-select text-align-center" style=width:auto height: auto;>
-                                                <option selected>Tipo de vaso</option>
-                                                <option value="1">Grande</option>
-                                                <option value="2">Mediano</option>
-                                                <option value="3">Puqueño</option>
-                                            </select> -->
-                                            <a style="text-decoration: none;" href="carrito.php">
-                                                <p class="fs-3 md-5 text-dark"> Agregar al carrito</p>
-                                            </a>
-                                            <h5 class="fs-5">Precio: $40.00 - $80.00</h5>
-                                        </div>
-
-                                        <!-- Modal footer -->
-                                        <div class="modal-footer">
-                                            <button class="btn btn-danger" data-bs-dismiss="modal">
-                                                Cerrar
-                                            </button>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Fin del modal -->
                             <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
@@ -126,52 +207,6 @@
                             <button class="btnn btnn-two ">
                                 <p class="text-boton">Ver más</p>
                             </button>
-                            <!-- Inicio de modal -->
-                            <div class="modal fade" id="myModal2">
-                                <div class="modal-dialog modal-xl modal-dialog-centered">
-                                    <div class="modal-content">
-
-                                        <!-- titulo del modal -->
-                                        <div class="modal-header">
-                                            <h2 class="modal-tittle ">
-                                                Jugo
-                                            </h2>
-                                            <button class="btn btn-close" data-bs-dismiss="modal"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <h2 class="fs-3 col-md-5">Jugo de manzana</h2>
-                                            <!-- <a href="carrito.php"> -->
-                                            <img class="rounded float-start" src="../IMG/jugoManzana.jpg" alt="Una imagen de fresa">
-                                            <!-- </a> -->
-                                            <h1 class="fs-4">Descripción</h1>
-                                            <p class="f-3">
-                                                Jugo muy bueno
-                                            </p>
-                                            <!-- Selector, no puedo moverlo al centro  -->
-                                            <!-- <select class="position-relative form-select text-align-center" style=width:auto height: auto;>
-                                                <option selected>Tipo de vaso</option>
-                                                <option value="1">Grande</option>
-                                                <option value="2">Mediano</option>
-                                                <option value="3">Puqueño</option>
-                                            </select> -->
-                                            <a style="text-decoration: none;" href="carrito.php">
-                                                <p class="fs-3 md-5 text-dark"> Agregar al carrito</p>
-                                            </a>
-                                            <h5 class="fs-5"><span class="text-muted text-decoration-line-through">$20.00</span>
-                                                $18.00</h5>
-                                        </div>
-
-                                        <!-- Modal footer -->
-                                        <div class="modal-footer">
-                                            <button class="btn btn-danger" data-bs-dismiss="modal">
-                                                Cerrar
-                                            </button>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Fin del modal -->
                             <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
@@ -201,51 +236,6 @@
                             <button class="btnn btnn-two ">
                                 <p class="text-boton">Ver más</p>
                             </button>
-                            <!-- Inicio de modal -->
-                            <div class="modal fade" id="myModal3">
-                                <div class="modal-dialog modal-xl modal-dialog-centered">
-                                    <div class="modal-content">
-
-                                        <!-- titulo del modal -->
-                                        <div class="modal-header">
-                                            <h2 class="modal-tittle text-center">
-                                                Jugo
-                                            </h2>
-                                            <button class="btn btn-close" data-bs-dismiss="modal"></button>
-                                        </div>
-                                        <div class="modal-body">
-                                            <h2 class="fs-3 col-md-12">Jugo combinado</h2>
-                                            <!-- <a href="carrito.php"> -->
-                                            <img class="rounded float-start img-fluid" src="../IMG/jugoConvinado.jpg" alt="Una imagen de fresa">
-                                            <!-- </a> -->
-                                            <h1 class="fs-4">Descripción</h1>
-                                            <p class="f-3">
-                                                Jugo muy bueno
-                                            </p>
-                                            <!-- Selector, no puedo moverlo al centro  -->
-                                            <!-- <select class="position-relative form-select text-align-center" style=width:auto height: auto;>
-                                                <option selected>Tipo de vaso</option>
-                                                <option value="1">Grande</option>
-                                                <option value="2">Mediano</option>
-                                                <option value="3">Puqueño</option>
-                                            </select> -->
-                                            <a style="text-decoration: none;" href="carrito.php">
-                                                <p class="fs-3 md-5 text-dark"> Agregar al carrito</p>
-                                            </a>
-                                            <h5 class="fs-5">$40.00</h5>
-                                        </div>
-
-                                        <!-- Modal footer -->
-                                        <div class="modal-footer">
-                                            <button class="btn btn-danger" data-bs-dismiss="modal">
-                                                Cerrar
-                                            </button>
-                                        </div>
-
-                                    </div>
-                                </div>
-                            </div>
-                            <!-- Fin del modal -->
                             <button type="button" class="btn btn-light " style="text-decoration: none"><a href="carrito.php">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
                                         <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z" />
